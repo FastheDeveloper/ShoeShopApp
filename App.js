4bfa2de1803736b67a8c598e4cf8591a1630cb56
FastheDeveloper <109018244+FastheDeveloper@users.noreply.github.com>
2023-03-18 15:40:23 +0100

    Update App.js

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { Provider } from 'react-redux';
 import { store } from './src/Redux Store';
 import { StripeProvider } from '@stripe/stripe-react-native';
 
-const STRIPE_KEY='pk_test_51Mmy8ICMK9tGvZA1I5JO1xqOxdEv5jXj2sqExtLcL7dIpFZTH3PyDt9Wvbnmv0lq65Q7FItjRd53E2Q9no7KZiSi00j9IHuGLq'
+const STRIPE_KEY='STRIPE_PUBLISHABLE_KEY'
 export default function App() {
   return (
     <Provider store={store}>
